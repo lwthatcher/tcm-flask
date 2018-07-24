8412bb47e367b3d9c904f73255d082addcd4bc66
Lawrence Thatcher <lwthatcher@msn.com>
2018-07-23 19:59:06 -0600

    app creation done in  a function

--- server/server.py
+++ server/server.py
@@ -4,23 +4,25 @@ import sys
 def create_app(stuff):
     print('creating app', stuff)
     app = Flask(__name__, static_url_path='/users/data/workspaces')
+
+    @app.route('/')
+    def index():
+        return 'Index Page'
+
+    @app.route('/hello')
+    def hello():
+        return url_for('static', filename='racquetball/racquetball.workspace.json')
+
+    @app.route('/static/<path:path>')
+    def resource(path):
+        _p = url_for('static', filename=path)
+        print('RESOURCE', _p)
+        return send_file(_p)
+
     return app
 
 app = create_app('buffer')
 
-@app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route('/hello')
-def hello():
-    return url_for('static', filename='racquetball/racquetball.workspace.json')
-
-@app.route('/static/<path:path>')
-def resource(path):
-    _p = url_for('static', filename=path)
-    print('RESOURCE', _p)
-    return send_file(_p)
 
 def run():
     print('sys args', sys.argv)
